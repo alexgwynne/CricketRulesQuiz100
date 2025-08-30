cd2c5851e9d5d5b3972b5c1f8b435edc73bcd27e
alexgwynne <alex.gwynne2974@gmail.com>
2025-08-30 13:31:06 +1200

    Added a welcome message to the quiz

---
+++ CricketRulesQuiz.py
@@ -0,0 +1,6 @@
+#Cricket Rules Quiz
+#By Alex Gwynne
+
+print("Welcome to the best quiz for learning the rules of cricket!")
+print("This quiz is suitable for people of varying experience playing cricket")
+print("Hopefully you will find this quiz helpful whether you are a player, coach or umpire")
