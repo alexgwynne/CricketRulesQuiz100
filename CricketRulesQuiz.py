fd256dfc358c0377df6e90446c5e7bb067024ebe
alexgwynne <alex.gwynne2974@gmail.com>
2025-09-01 11:41:24 +1200

    Changes Made -  Added variables

--- CricketRulesQuiz.py
+++ CricketRulesQuiz.py
@@ -1,6 +1,41 @@
 #Cricket Rules Quiz
 #By Alex Gwynne
 
+print('hi')
+
+#variables
+score = 0
+score1 = 0
+score2 = 0
+score3 = 0
+
+easyquestions = (
+
+)
+hardquestions = (
+
+)
+triviaquestions = (
+
+)
+lingoquestions = (
+
+)
+
+
+optionseasy = (
+
+)
+optionshard = (
+
+)
+optionstrivia = (
+
+)
+optionslingo = (
+    
+)
+
 print("Welcome to the best quiz for learning the rules of cricket!")
 print("This quiz is suitable for people of varying experience playing cricket")
 print("Hopefully you will find this quiz helpful whether you are a player, coach or umpire")
