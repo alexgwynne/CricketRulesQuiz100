819e41622159338ff4adcaba075f1b6d65b8ce87
alexgwynne <alex.gwynne2974@gmail.com>
2025-09-08 11:51:37 +1200

    test

--- CricketRulesQuiz.py
+++ CricketRulesQuiz.py
@@ -1,17 +1,19 @@
 #Cricket Rules Quiz
 #By Alex Gwynne
 
-print('hi')
-
 #variables
 score = 0
 score1 = 0
 score2 = 0
 score3 = 0
 
-easyquestions = (
+def easyrulesquiz ():
+    easyquestions = [
 
-)
+
+    ]
+
+    
 hardquestions = (
 
 )
@@ -39,3 +41,7 @@ optionslingo = (
 print("Welcome to the best quiz for learning the rules of cricket!")
 print("This quiz is suitable for people of varying experience playing cricket")
 print("Hopefully you will find this quiz helpful whether you are a player, coach or umpire")
+name = input("What is your name? ")
+print(f"Welcome {name}. Let's play a quiz!")
+
+print('hi')
