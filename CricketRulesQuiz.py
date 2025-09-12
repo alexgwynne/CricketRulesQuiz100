3d3cdac9cd1023481c9e5adcd3ba30b47d63563d
alexgwynne <alex.gwynne2974@gmail.com>
2025-09-13 10:39:25 +1200

    Added the questions for the first quiz which is the easier of the two rules quizzes

--- CricketRulesQuiz.py
+++ CricketRulesQuiz.py
@@ -2,47 +2,26 @@
 #By Alex Gwynne
 
 #variables
-score = 0
-score1 = 0
-score2 = 0
-score3 = 0
-
-def easyrulesquiz ():
-    easyquestions = [
-
-
+quiz_options = {
+    "EasyQuiz": [
+        {"question": "Which of the following situations constitutes a ‘wide ball’ signal from the umpire?", "options": ["", "", ""], "answer": ""},
+        {"question": "Which of the following situations constitutes a ‘no ball’ signal from the umpire?", "options": ["", "", ""], "answer": ""},
+        {"question": "If the batsman hits the ball over the boundary on the full, how many runs are awarded?", "options": ["", "", ""], "answer": ""},
+        {"question": "If the batsman hits the ball to the boundary along the ground, how many runs are awarded?", "options": ["", "", ""], "answer": ""},
+        {"question": "What does LBW stand for?" "options": ["", "", ""], "answer": ""},
+        {"question": "How many players from the fielding team can be on the field at once?", "options": ["", "", ""], "answer": ""},
+        {"question": "Apart from boundaries, how else can the batsman score runs?", "options": ["", "", ""], "answer": ""},
+        {"question": "How many deliveries are in an over?", "options": ["", "", ""], "answer": ""},
+        {"question": "What is the maximum number of fielders the fielding team can have behind the 90-degree angle on the leg side of the pitch?", "options": ["", "", ""], "answer": ""},
+        {"question": "How many umpires are there for a cricket match?", "options": ["", "", ""], "answer": ""},
     ]
-
-    
-hardquestions = (
-
-)
-triviaquestions = (
-
-)
-lingoquestions = (
-
-)
+}
 
 
-optionseasy = (
-
-)
-optionshard = (
-
-)
-optionstrivia = (
-
-)
-optionslingo = (
-    
-)
 
 print("Welcome to the best quiz for learning the rules of cricket!")
 print("This quiz is suitable for people of varying experience playing cricket")
 print("Hopefully you will find this quiz helpful whether you are a player, coach or umpire")
+While True
 name = input("What is your name? ")
 print(f"Welcome {name}. Let's play a quiz!")
-
-print('hi')
-print('hi')
